146db8bd75ff99310a842a9c8083f7265c277615
teja463 <teja463@gmail.com>
2018-03-18 21:51:00 +0530

    added courseera tutorial project

---
+++ courseera/conFusion/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatToolbarModule } from '@angular/material';
+import 'hammerjs';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
